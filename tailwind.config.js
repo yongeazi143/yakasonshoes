6d50aa9abc0debcffa1721866bee8d4d84c13401
yongeazi143 <52831161+yongeazi143@users.noreply.github.com>
2022-12-17 16:42:48 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html','.build/js/*.js'],
+  theme: {
+    color: {
+      'dark-brown': '#694A26',
+      'light-brown': '#9C6B4C',
+      'arrow-town': '#9C855D',
+      'domino': '#907C5C',
+      'mint-cream': '#F7FFF7'
+      
+    },
+  },
+  plugins: [],
+}
