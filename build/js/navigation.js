ea0f74b74c20a12ba3c7731923651b0cc042839e
yongeazi143 <52831161+yongeazi143@users.noreply.github.com>
2022-12-27 11:58:39 +0100

    Add files via upload

--- build/js/navigation.js
+++ build/js/navigation.js
@@ -1,30 +1,65 @@
 const navToggle = () => {
-   const bugger = document.querySelector('#nav-toggle');
-   const closeNav = document.querySelector('#close-nav');
-   const mobileNav = document.querySelector('#mobile-nav');
+  const bugger = document.querySelector("#nav-toggle");
+  const closeNav = document.querySelector("#close-nav");
+  const mobileNav = document.querySelector("#mobile-nav");
+  // let isFalse = false;
+  bugger.addEventListener("click", () => {
+    mobileNav.classList.toggle("close");
+    mobileNav.classList.toggle("open");
+  });
 
-   bugger.addEventListener('click', () => {
-      mobileNav.classList.toggle('sidebar');
-   });
+  // closeNav.addEventListener('click', () => {
+  //    // mobileNav.classList.remove('sidebar')
+  //    if (!isFalse) {
+  //    mobileNav.classList.remove('close transition-all');
+  //    mobileNav.classList.add('open transition-all')
 
-   closeNav.addEventListener('click', () => {
-      mobileNav.classList.remove('sidebar')
-   });
-   
-   window.addEventListener("resize", function () {
-      if (window.innerWidth >= 768) {
-        mobileNav.classList.remove('sidebar')
-      }
-    });
-}
+  //    }
+  // });
+
+  // window.addEventListener("resize", function () {
+  //    if (window.innerWidth >= 768) {
+  //      mobileNav.classList.toggle('')
+  //    }
+  //  });
+};
 
 navToggle();
 
 const dropdownToggle = () => {
-   const mobileDropDown = document.getElementsByClassName('.accordion');
-   const content = document.querySelectorAll('.panel');
-   console.log(mobileDropDown)
+  let active = true;
 
-   
-}
+  const dropServices = document.querySelector("#drop-services");
+  const servicesContent = document.querySelector("#services-content");
+  const dropProduct = document.querySelector("#drop-products");
+  const productContent = document.querySelector("#products-content");
+  dropServices.addEventListener("click", function () {
+    // services
+    if (active) {
+      console.log(true);
+      dropServices.classList.toggle("bg-amber-700");
+      servicesContent.classList.toggle("nav-close");
+      servicesContent.classList.toggle("nav-open");
+      // dropProduct.addEventListener("click", function () {
+      //    // console.log(false)
+      // dropServices.classList.toggle("bg-amber-700");
+      //   servicesContent.classList.toggle("nav-close");
+      //   servicesContent.classList.toggle("nav-open");
+      // });
+      active = false;
+    } else {
+      dropServices.classList.toggle("bg-amber-700");
+      console.log(false);
+      servicesContent.classList.toggle("nav-close");
+      servicesContent.classList.toggle("nav-open");
+      active = true;
+    }
+  });
+  dropProduct.addEventListener("click", function () {
+    //  product
+    dropProduct.classList.toggle("bg-amber-700");
+    productContent.classList.toggle("nav-close");
+    productContent.classList.toggle("nav-open");
+  });
+};
 dropdownToggle();
