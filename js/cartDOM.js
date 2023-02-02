0c4de976d1107af9992b060b99c9dfdec356cbff
yongeazi143 <52831161+yongeazi143@users.noreply.github.com>
2023-02-03 00:47:26 +0100

    Delete js directory

--- js/cartDOM.js
+++
@@ -1,9 +0,0 @@
-const proContainer = document.querySelector("#pro-container");
-const cartQuantity = document.querySelector("#quantity");
-
-export { proContainer, cartQuantity };
-
-// import { Display } from "./add_to_cart.js";
-// //
-// let display = new Display();
-// display.cartValues(cart);
